23e7ec7f6d37eb782a17a16b4c28133ef1d1aad5
Eun Huisu <91324571+hesuhesu@users.noreply.github.com>
2023-05-25 22:55:43 +0900

    Update open.js

--- js/open.js
+++ js/open.js
@@ -1,11 +1,15 @@
 while (true){
-    var name = prompt("이름을 입력하세요.");
+    var names = prompt("이름을 입력하세요.");
 
-    if (name == "admin"){
+    if (names == "admin"){
+        break;
+    }
+    else if (names == null){
+        location.href = "html/again.html"
         break;
     }
     else {
         alert("다시 입력하세요.");
     }
 }
-document.write("<b><big>" + name + "</big></b>님, 환영합니다.");
+document.write("<b><big>" + names + "</big></b>님, 환영합니다.");
